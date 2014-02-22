50fd570c1a161a27c395da3934764398adff3a9f
unknown <kunalgrover05@gmail.com>
2014-02-22 08:55:34 +0530

    First version
    Downloads most files and folder content from moodle. Reports if files downloaded are not equal to no of files on website

---
+++ moodle-downloader.py
@@ -0,0 +1,124 @@
+import cookielib
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+import os.path
+import os
+import filecmp
+import sys
+
+global i
+global directory
+
+def save_file( data, filename ) :
+    global i
+    global directory
+    
+    filename = directory + "\\" + filename
+    filename = urllib.unquote( filename )
+    tempfile = directory + "\\temp"
+    
+    if not os.path.exists( directory ) :
+        os.makedirs( directory )
+
+    with open( tempfile, "wb" ) as code :
+        code.write( data )     
+    if os.path.isfile( filename ) :
+        if filecmp.cmp( filename, tempfile ) == True :
+            os.remove( tempfile )
+        else :
+            name_split = filename.split('.')
+            filename = name_split[0] + str(i) + "." + name_split[-1]
+            if os.path.isfile( filename ) :
+                if filecmp.cmp( filename, tempfile ) == True :
+                    os.remove( tempfile )
+            else:
+                os.rename( tempfile, filename )
+            i += 1
+    else:
+        os.rename( tempfile, filename )
+        
+def embedded_file( link ) :
+    filename = link.split('/')[-1]
+    data = urllib2.urlopen( link ).read()
+    save_file( data, filename )
+
+def direct_download( data ):
+    filename = data.info()['Content-Disposition'] . split( '=' )[-1] . strip( '"' )
+    save_file( data.read(), filename )
+
+def auth( username, password ):
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener( urllib2.HTTPCookieProcessor( cj ) )
+    urllib2.install_opener( opener )
+    authentication_url = 'https://courses.iitm.ac.in/login/index.php'
+    payload = {
+      'username' : username,
+      'password' : password
+      }
+    data = urllib.urlencode( payload )
+    req = urllib2.Request( authentication_url, data )
+    return req
+
+if __name__ == "__main__":
+    allsub = [ "MA2030" ,"AM2540" , "AM2530", "ME2280", "EN2100" , "ME2260" , "ME2220"  , "ME2240" ]
+    year = "2014"
+    username = ""
+    password = ""
+
+    os.chdir(os.path.dirname(sys.argv[0]))
+    
+    req = auth( username, password )
+    resp = urllib2.urlopen( req )
+    soup = BeautifulSoup( resp.read() )
+    
+    for link in soup.find_all( 'a' ) :
+        for subject in allsub :
+            if subject in str( link ) and year in str( link )  :
+                sub_link = link.get( 'href' )
+                print "Downloading " + subject
+                directory = subject + "-" + year
+                resp = urllib2.urlopen( sub_link )
+                soup = BeautifulSoup( resp.read() )
+                alldocs = list( set( soup.find_all('a') ) )
+                
+                total = 0
+                i = 0
+
+                for doc in alldocs :
+                    if 'mod/resource' in doc.get( 'href' ):
+                        total += 1
+
+                        doc_click = urllib2.urlopen( doc.get( 'href' ) )
+
+                        if 'Content-Disposition' in str( doc_click.info() ) :
+                            direct_download( doc_click )
+                        
+                        else :
+                            soup1 = BeautifulSoup( doc_click.read() )
+                            page_links = soup1.find_all( 'a' )
+                            page_images = soup1.findAll( 'img' )
+                            for img in page_images :
+                                img_links = img.get( 'src' )
+                                if '.jpeg' in str( img_links ).lower() or '.jpg' in str( img_links ).lower() :
+                                    embedded_file( img_links )
+                            for links in page_links :
+                                doc_links = links.get( 'href' )
+                                if 'pdf' in str( doc_links ).lower() or 'doc' in str( doc_links ).lower():
+                                    embedded_file( doc_links )
+
+                    elif 'mod/folder' in doc.get('href') :
+                        doc_click = urllib2.urlopen( doc.get ( 'href' ) )
+                        soup1 = BeautifulSoup( doc_click.read() )
+                        page_links = soup1.find_all( 'a' )
+                        for link in page_links:
+                            if 'download' in link.get( 'href' ):
+                                total += 1
+                                link_click = urllib2.urlopen( link.get( 'href' ) )
+                                if 'Content-Disposition' in str( link_click.info() ) :
+                                    direct_download( link_click )
+                                        
+                if os.path.exists( directory ) and len( os.listdir( directory ) ) != total:
+                   print subject + ": Go to moodle and check if some files not downloaded" 
+                else:
+                    print subject+": done"
